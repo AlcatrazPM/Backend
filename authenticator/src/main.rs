899c3977fae2466f5e5c7383a18de1ea5a7fb9ae
Adrian Scrab <adrian.scrab@pm.me>
2020-04-12 11:15:48 +0300

    Added tracking for main and cargo.toml
    whyyyyyyyyyyyy didn't they track?

---
+++ authenticator/src/main.rs
@@ -0,0 +1,19 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[macro_use(bson, doc)]
+extern crate bson;
+
+use crate::appserver::AppServer;
+
+mod appserver;
+mod authenticator_control;
+mod dataprovider;
+mod jwt;
+mod userdata;
+
+fn main() {
+    AppServer::run();
+}
